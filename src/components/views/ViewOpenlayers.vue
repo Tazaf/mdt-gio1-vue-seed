b017b3698395b7b02f4121cf2a4c9ef39720bcfc
BDC <36916497+Brounst176@users.noreply.github.com>
2022-12-15 17:12:45 +0100

    modif

--- src/components/views/ViewOpenlayers.vue
+++ src/components/views/ViewOpenlayers.vue
@@ -5,55 +5,75 @@
 <script>
 import 'ol/ol.css';
 import Map from 'ol/Map';
-import OSM from 'ol/source/OSM';
+import WMTS from 'ol/source/WMTS';
 import TileLayer from 'ol/layer/Tile';
-import View from 'ol/View';
-import * as olProj from 'ol/proj';
+import source from 'ol/source';
+import tilegrid from 'ol/tilegrid/WMTS';
+import * as olProj from 'ol/proj/';
+import { register } from 'ol/proj/proj4';
+import proj4 from 'proj4';
+import View from "ol/View"
+
+// import { register } from 'jscodeshift/src/collections/JSXElement';
+
+//Definition de la projection
+
+proj4.defs("EPSG:2056", "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs");
+
+register(proj4)
+
+const resolutions = [
+  4000, 3750, 3500, 3250, 3000, 2750, 2500, 2250, 2000, 1750, 1500, 1250,
+  1000, 750, 650, 500, 250, 100, 50, 20, 10, 5, 2.5, 2, 1.5, 1, 0.5
+];
+
+const extent = [2420000, 130000, 2900000, 1350000];
+const projection = olProj.get('EPSG:2056');
+// console.log(projection)
+projection.setExtent(extent);
+
+const matrixIds = [];
+for (var i = 0; i < resolutions.length; i++) {
+  matrixIds.push(i);
+}
+
+
+// Ajout fond de carte
+const wmtsCadastre = new TileLayer({
+  source: new WMTS(({
+    layer: 'asitvd.fond_cadastral',
+    crossOrigin: 'anonymous',
+    url: '//wmts.asit-asso.ch/wmts/1.0.0/{Layer}/default/default/0/2056/{TileMatrix}/{TileRow}/{TileCol}.png',
+    tileGrid: new tilegrid({
+      origin: [extent[0], extent[3]],
+      resolutions: resolutions,
+      matrixIds: matrixIds
+    }),
+    requestEncoding: 'REST'
+  })),
+  visible: true
+});
+
+const view_map = new View({
+  center: [2519500, 1149600],
+  projection: projection,
+  zoom: 7,
+  maxZoom: 16
+});
+
 
 export default {
-  data() {
-    return {
-      center: [6.659361, 46.779389],
-      olmap: null,
-      zoom: 17
-    };
-  },
-  computed: {
-    /**
-     * Transform coordinate from EPSG:4326 to EPSG:3857
-     *
-     * @use center in EPSG:4326
-     * @return center in EPSG:3857
-     */
-    center3857() {
-      return olProj.transform(this.center, 'EPSG:4326', 'EPSG:3857');
-    }
-  },
-  methods: {
-    /**
-     * Init Openlayers map
-     *
-     * @param {number[]} mapcenter center of the map in EPSG:3857
-     * @param {number} mapzoom zommlevel
-     * @returns {Map} initmap new openlayers map
-     */
-    setupOpenlayersMap(mapcenter, mapzoom) {
-      return new Map({
-        target: 'ol-container',
-        layers: [
-          new TileLayer({
-            source: new OSM()
-          })
-        ],
-        view: new View({
-          center: mapcenter,
-          zoom: mapzoom
-        })
-      });
-    }
-  },
+
   mounted() {
-    this.olmap = this.setupOpenlayersMap(this.center3857, this.zoom);
+    const map = new Map({
+      target: "ol-container",
+      view: view_map,
+
+    });
+
+    map.addLayer(wmtsCadastre);
+
+
   }
 };
 </script>
